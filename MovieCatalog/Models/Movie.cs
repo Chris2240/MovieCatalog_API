aef9a0a252716739ea31a57768f2e8ab49ae9c6f
Chris2240 <kris2240.kp@gmail.com>
2025-09-11 22:40:48 +0100

    Creating folder "Models" and "Movie" class which represent properties of "Movie" class

---
+++ MovieCatalog/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieCatalog.Models
+{
+    public class Movie
+    {
+        // Properties of the Movie class
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Genre { get; set; } = "";
+        public int Year { get; set; }
+        public double Rating { get; set; }  // e.g., 4.5 rating
+    }
+}
